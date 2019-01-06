f7ca1bf0ab54a8aa1ff1759ab87c4f97732851c0
Sherman The Tank <5414276+sherman-the-tank@users.noreply.github.com>
2019-01-06 20:26:06 +0800

    Moved wal out of raftex, so that it can be used by other modules (#62)

---
+++ src/common/LogIterator.h
@@ -0,0 +1,33 @@
+/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+ *
+ * This source code is licensed under Apache 2.0 License
+ *  (found in the LICENSE.Apache file in the root directory)
+ */
+
+#ifndef COMMON_BASE_LOGITERATOR_H_
+#define COMMON_BASE_LOGITERATOR_H_
+
+#include "base/Base.h"
+
+namespace nebula {
+
+class LogIterator {
+public:
+    virtual ~LogIterator() = default;
+
+    virtual LogIterator& operator++() = 0;
+
+    virtual bool valid() const = 0;
+    virtual operator bool() const {
+        return valid();
+    }
+
+    virtual LogID logId() const = 0;
+    virtual TermID logTerm() const = 0;
+    virtual ClusterID logSource() const = 0;
+    virtual folly::StringPiece logMsg() const = 0;
+};
+
+}  // namespace nebula
+#endif  // COMMON_BASE_LOGITERATOR_H_
+
