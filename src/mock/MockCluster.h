3620551776cc8a42ffe95f9e456664157ecc7ad5
heng <heng.chen@vesoft.com>
2020-04-10 21:12:08 +0800

    Add mock server

--- src/mock/MockCluster.h
+++ src/mock/MockCluster.h
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 /* Copyright (c) 2020 vesoft inc. All rights reserved.
+=======
+/* Copyright (c) 2018 vesoft inc. All rights reserved.
+>>>>>>> Add mock server
  *
  * This source code is licensed under Apache 2.0 License,
  * attached with Common Clause Condition 1.0, found in the LICENSES directory.
