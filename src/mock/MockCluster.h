a381b201b2312b9c3cd5757e4a7ac9b605ca6446
heng <heng.chen@vesoft.com>
2020-04-14 10:34:13 +0800

    address panda-sheep's comments

--- src/mock/MockCluster.h
+++ src/mock/MockCluster.h
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 /* Copyright (c) 2020 vesoft inc. All rights reserved.
-=======
-/* Copyright (c) 2018 vesoft inc. All rights reserved.
->>>>>>> Add mock server
  *
  * This source code is licensed under Apache 2.0 License,
  * attached with Common Clause Condition 1.0, found in the LICENSES directory.
