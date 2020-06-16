4d2a095f2414614f7ffe5d58e992f3e3c26caecd
bright-starry-sky <sky.chen@vesoft.com>
2020-06-16 19:46:07 +0800

    make code more beautiful for IndexKeyUtils

--- src/utils/IndexKeyUtils.cpp
+++ src/utils/IndexKeyUtils.cpp
@@ -56,59 +56,230 @@ void IndexKeyUtils::encodeValuesWithNull(const std::vector<Value>& values,
 }
 
 // static
-std::string IndexKeyUtils::vertexIndexKey(size_t vIdLen, PartitionID partId,
-                                          IndexID indexId, VertexID vId,
-                                          const std::vector<Value>& values,
-                                          const std::vector<Value::Type>& valueTypes) {
-    int32_t item = (partId << kPartitionOffset) | static_cast<uint32_t>(NebulaKeyType::kIndex);
-    std::string key;
-    key.reserve(256);
-    key.append(reinterpret_cast<const char*>(&item), sizeof(int32_t))
-       .append(reinterpret_cast<const char*>(&indexId), sizeof(IndexID));
-    // If have not nullable columns in the index, the valueTypes is empty.
-    if (valueTypes.empty()) {
-        encodeValues(values, key);
-    } else {
-        encodeValuesWithNull(values, valueTypes, key);
+Value::Type IndexKeyUtils::toValueType(PropertyType type) {
+    switch (type) {
+        case PropertyType::BOOL :
+            return Value::Type::BOOL;
+        case PropertyType::INT64 :
+        case PropertyType::INT32 :
+        case PropertyType::INT16 :
+        case PropertyType::INT8 :
+        case PropertyType::TIMESTAMP :
+            return Value::Type::INT;
+        case PropertyType::VID :
+            return Value::Type::VERTEX;
+        case PropertyType::FLOAT :
+        case PropertyType::DOUBLE :
+            return Value::Type::FLOAT;
+        case PropertyType::STRING :
+        case PropertyType::FIXED_STRING :
+            return Value::Type::STRING;
+        case PropertyType::DATE :
+            return Value::Type::DATE;
+        case PropertyType::DATETIME :
+            return Value::Type::DATETIME;
+        case PropertyType::UNKNOWN :
+            return Value::Type::__EMPTY__;
     }
-    key.append(vId.data(), vId.size())
-       .append(vIdLen - vId.size(), '\0');
-    return key;
+    return Value::Type::__EMPTY__;
 }
 
 // static
-std::string IndexKeyUtils::edgeIndexKey(size_t vIdLen, PartitionID partId,
-                                        IndexID indexId, VertexID srcId,
-                                        EdgeRanking rank, VertexID dstId,
-                                        const std::vector<Value>& values,
-                                        const std::vector<Value::Type>& valueTypes) {
-    int32_t item = (partId << kPartitionOffset) | static_cast<uint32_t>(NebulaKeyType::kIndex);
-    std::string key;
-    key.reserve(256);
-    key.append(reinterpret_cast<const char*>(&item), sizeof(int32_t))
-       .append(reinterpret_cast<const char*>(&indexId), sizeof(IndexID));
-    // If have not nullable columns in the index, the valueTypes is empty.
-    if (valueTypes.empty()) {
-        encodeValues(values, key);
-    } else {
-        encodeValuesWithNull(values, valueTypes, key);
+std::string IndexKeyUtils::encodeNullValue(Value::Type type) {
+    size_t len = 0;
+    switch (type) {
+        case Value::Type::INT : {
+            len = sizeof(int64_t);
+            break;
+        }
+        case Value::Type::FLOAT : {
+            len = sizeof(double);
+            break;
+        }
+        case Value::Type::BOOL: {
+            len = sizeof(bool);
+            break;
+        }
+        case Value::Type::STRING : {
+            len = 1;
+            break;
+        }
+        case Value::Type::DATE : {
+            len = sizeof(int8_t) * 2 + sizeof(int16_t);
+            break;
+        }
+        case Value::Type::DATETIME : {
+            len = sizeof(int32_t) * 2 + sizeof(int16_t) + sizeof(int8_t) * 5;
+            break;
+        }
+        default :
+            LOG(ERROR) << "Unsupported default value type";
     }
-    key.append(srcId.data(), srcId.size())
-       .append(vIdLen - srcId.size(), '\0')
-       .append(reinterpret_cast<const char*>(&rank), sizeof(EdgeRanking))
-       .append(dstId.data(), dstId.size())
-       .append(vIdLen - dstId.size(), '\0');
-    return key;
+    std::string raw;
+    raw.reserve(len);
+    raw.append(len, '\0');
+    return raw;
 }
 
 // static
-std::string IndexKeyUtils::indexPrefix(PartitionID partId, IndexID indexId) {
-    PartitionID item = (partId << kPartitionOffset) | static_cast<uint32_t>(NebulaKeyType::kIndex);
-    std::string key;
-    key.reserve(sizeof(PartitionID) + sizeof(IndexID));
-    key.append(reinterpret_cast<const char*>(&item), sizeof(PartitionID))
-       .append(reinterpret_cast<const char*>(&indexId), sizeof(IndexID));
-    return key;
+std::string IndexKeyUtils::encodeValue(const Value& v) {
+    switch (v.type()) {
+        case Value::Type::INT :
+            return encodeInt64(v.getInt());
+        case Value::Type::FLOAT :
+            return encodeDouble(v.getFloat());
+        case Value::Type::BOOL: {
+            auto val = v.getBool();
+            std::string raw;
+            raw.reserve(sizeof(bool));
+            raw.append(reinterpret_cast<const char*>(&val), sizeof(bool));
+            return raw;
+        }
+        case Value::Type::STRING :
+            return v.getStr();
+        case Value::Type::DATE : {
+            std::string buf;
+            buf.reserve(sizeof(int8_t) * 2 + sizeof(int16_t));
+            buf.append(reinterpret_cast<const char*>(&v.getDate().year), sizeof(int16_t))
+               .append(reinterpret_cast<const char*>(&v.getDate().month), sizeof(int8_t))
+               .append(reinterpret_cast<const char*>(&v.getDate().day), sizeof(int8_t));
+            return buf;
+        }
+        case Value::Type::DATETIME : {
+            std::string buf;
+            buf.reserve(sizeof(int32_t) * 2 + sizeof(int16_t) + sizeof(int8_t) * 5);
+            auto dt = v.getDateTime();
+            buf.append(reinterpret_cast<const char*>(&dt.year), sizeof(int16_t))
+               .append(reinterpret_cast<const char*>(&dt.month), sizeof(int8_t))
+               .append(reinterpret_cast<const char*>(&dt.day), sizeof(int8_t))
+               .append(reinterpret_cast<const char*>(&dt.hour), sizeof(int8_t))
+               .append(reinterpret_cast<const char*>(&dt.minute), sizeof(int8_t))
+               .append(reinterpret_cast<const char*>(&dt.sec), sizeof(int8_t))
+               .append(reinterpret_cast<const char*>(&dt.microsec), sizeof(int32_t))
+               .append(reinterpret_cast<const char*>(&dt.timezone), sizeof(int32_t));
+            return buf;
+        }
+        default :
+            LOG(ERROR) << "Unsupported default value type";
+    }
+    return "";
+}
+
+// static
+std::string IndexKeyUtils::encodeInt64(int64_t v) {
+    v ^= folly::to<int64_t>(1) << 63;
+    auto val = folly::Endian::big(v);
+    std::string raw;
+    raw.reserve(sizeof(int64_t));
+    raw.append(reinterpret_cast<const char*>(&val), sizeof(int64_t));
+    return raw;
+}
+
+// static
+int64_t IndexKeyUtils::decodeInt64(const folly::StringPiece& raw) {
+    auto val = *reinterpret_cast<const int64_t*>(raw.data());
+    val = folly::Endian::big(val);
+    val ^= folly::to<int64_t>(1) << 63;
+    return val;
+}
+
+// static
+std::string IndexKeyUtils::encodeDouble(double v) {
+    if (v < 0) {
+        /**
+         *   TODO : now, the -(std::numeric_limits<double>::min())
+         *   have a problem of precision overflow. current return value is -nan.
+         */
+        auto i = *reinterpret_cast<const int64_t*>(&v);
+        i = -(std::numeric_limits<int64_t>::max() + i);
+        v = *reinterpret_cast<const double*>(&i);
+    }
+    auto val = folly::Endian::big(v);
+    auto* c = reinterpret_cast<char*>(&val);
+    c[0] ^= 0x80;
+    std::string raw;
+    raw.reserve(sizeof(double));
+    raw.append(c, sizeof(double));
+    return raw;
+}
+
+// static
+double IndexKeyUtils::decodeDouble(const folly::StringPiece& raw) {
+    char* v = const_cast<char*>(raw.data());
+    v[0] ^= 0x80;
+    auto val = *reinterpret_cast<const double*>(v);
+    val = folly::Endian::big(val);
+    if (val < 0) {
+        auto i = *reinterpret_cast<const int64_t*>(&val);
+        i = -(std::numeric_limits<int64_t >::max() + i);
+        val = *reinterpret_cast<const double*>(&i);
+    }
+    return val;
+}
+
+// static
+StatusOr<Value> IndexKeyUtils::decodeValue(const folly::StringPiece& raw, Value::Type type) {
+    Value v;
+    switch (type) {
+        case Value::Type::INT : {
+            v.setInt(decodeInt64(raw));
+            break;
+        }
+        case Value::Type::FLOAT : {
+            v.setFloat(decodeDouble(raw));
+            break;
+        }
+        case Value::Type::BOOL : {
+            v.setBool(*reinterpret_cast<const bool*>(raw.data()));
+            break;
+        }
+        case Value::Type::STRING : {
+            v.setStr(raw.str());
+            break;
+        }
+        case Value::Type::DATE: {
+            nebula::Date dt;
+            memcpy(reinterpret_cast<void*>(&dt.year), &raw[0], sizeof(int16_t));
+            memcpy(reinterpret_cast<void*>(&dt.month),
+                    &raw[sizeof(int16_t)],
+                    sizeof(int8_t));
+            memcpy(reinterpret_cast<void*>(&dt.day),
+                    &raw[sizeof(int16_t) + sizeof(int8_t)],
+                    sizeof(int8_t));
+            v.setDate(dt);
+            break;
+        }
+        case Value::Type::DATETIME: {
+            nebula::DateTime dt;
+            memcpy(reinterpret_cast<void*>(&dt.year), &raw[0], sizeof(int16_t));
+            memcpy(reinterpret_cast<void*>(&dt.month),
+                    &raw[sizeof(int16_t)],
+                    sizeof(int8_t));
+            memcpy(reinterpret_cast<void*>(&dt.day),
+                    &raw[sizeof(int16_t) + sizeof(int8_t)],
+                    sizeof(int8_t));
+            memcpy(reinterpret_cast<void*>(&dt.hour),
+                    &raw[sizeof(int16_t) + 2 * sizeof(int8_t)],
+                    sizeof(int8_t));
+            memcpy(reinterpret_cast<void*>(&dt.minute),
+                    &raw[sizeof(int16_t) + 3 * sizeof(int8_t)],
+                    sizeof(int8_t));
+            memcpy(reinterpret_cast<void*>(&dt.sec),
+                    &raw[sizeof(int16_t) + 4 * sizeof(int8_t)],
+                    sizeof(int8_t));
+            memcpy(reinterpret_cast<void*>(&dt.microsec),
+                    &raw[sizeof(int16_t) + 5 * sizeof(int8_t)],
+                    sizeof(int32_t));
+            memcpy(reinterpret_cast<void*>(&dt.timezone),
+                    &raw[sizeof(int16_t) + 5 * sizeof(int8_t) + sizeof(int32_t)],
+                    sizeof(int32_t));
+            v.setDateTime(dt);
+            break;
+        }
+        default:
+            return Status::Error("Unknown value type");
+    }
+    return v;
 }
 
 }  // namespace nebula
--- src/utils/IndexKeyUtils.h
+++ src/utils/IndexKeyUtils.h
@@ -23,242 +23,56 @@ class IndexKeyUtils final {
 public:
     ~IndexKeyUtils() = default;
 
-    static Value::Type toValueType(PropertyType type) {
-        switch (type) {
-            case PropertyType::BOOL :
-                return Value::Type::BOOL;
-            case PropertyType::INT64 :
-            case PropertyType::INT32 :
-            case PropertyType::INT16 :
-            case PropertyType::INT8 :
-            case PropertyType::TIMESTAMP :
-                return Value::Type::INT;
-            case PropertyType::VID :
-                return Value::Type::VERTEX;
-            case PropertyType::FLOAT :
-            case PropertyType::DOUBLE :
-                return Value::Type::FLOAT;
-            case PropertyType::STRING :
-            case PropertyType::FIXED_STRING :
-                return Value::Type::STRING;
-            case PropertyType::DATE :
-                return Value::Type::DATE;
-            case PropertyType::DATETIME :
-                return Value::Type::DATETIME;
-            case PropertyType::UNKNOWN :
-                return Value::Type::__EMPTY__;
+    /**
+     * param valueTypes ： column type of each index column. If there are no nullable columns
+     *                     in the index, the parameter can be empty.
+     **/
+    static std::string vertexIndexKey(size_t vIdLen, PartitionID partId,
+                                      IndexID indexId, VertexID vId,
+                                      const std::vector<Value>& values,
+                                      const std::vector<Value::Type>& valueTypes = {}) {
+        int32_t item = (partId << kPartitionOffset) | static_cast<uint32_t>(NebulaKeyType::kIndex);
+        std::string key;
+        key.reserve(256);
+        key.append(reinterpret_cast<const char*>(&item), sizeof(int32_t))
+           .append(reinterpret_cast<const char*>(&indexId), sizeof(IndexID));
+        // If have not nullable columns in the index, the valueTypes is empty.
+        if (valueTypes.empty()) {
+            encodeValues(values, key);
+        } else {
+            encodeValuesWithNull(values, valueTypes, key);
         }
-        return Value::Type::__EMPTY__;
-    }
-
-    static std::string encodeNullValue(Value::Type type) {
-        size_t len = 0;
-        switch (type) {
-            case Value::Type::INT : {
-                len = sizeof(int64_t);
-                break;
-            }
-            case Value::Type::FLOAT : {
-                len = sizeof(double);
-                break;
-            }
-            case Value::Type::BOOL: {
-                len = sizeof(bool);
-                break;
-            }
-            case Value::Type::STRING : {
-                len = 1;
-                break;
-            }
-            case Value::Type::DATE : {
-                len = sizeof(int8_t) * 2 + sizeof(int16_t);
-                break;
-            }
-            case Value::Type::DATETIME : {
-                len = sizeof(int32_t) * 2 + sizeof(int16_t) + sizeof(int8_t) * 5;
-                break;
-            }
-            default :
-                LOG(ERROR) << "Unsupported default value type";
-        }
-        std::string raw;
-        raw.reserve(len);
-        raw.append(len, '\0');
-        return raw;
-    }
-
-    static std::string encodeValue(const Value& v) {
-        switch (v.type()) {
-            case Value::Type::INT :
-                return encodeInt64(v.getInt());
-            case Value::Type::FLOAT :
-                return encodeDouble(v.getFloat());
-            case Value::Type::BOOL: {
-                auto val = v.getBool();
-                std::string raw;
-                raw.reserve(sizeof(bool));
-                raw.append(reinterpret_cast<const char*>(&val), sizeof(bool));
-                return raw;
-            }
-            case Value::Type::STRING :
-                return v.getStr();
-            case Value::Type::DATE : {
-                std::string buf;
-                buf.reserve(sizeof(int8_t) * 2 + sizeof(int16_t));
-                buf.append(reinterpret_cast<const char*>(&v.getDate().year), sizeof(int16_t))
-                   .append(reinterpret_cast<const char*>(&v.getDate().month), sizeof(int8_t))
-                   .append(reinterpret_cast<const char*>(&v.getDate().day), sizeof(int8_t));
-                return buf;
-            }
-            case Value::Type::DATETIME : {
-                std::string buf;
-                buf.reserve(sizeof(int32_t) * 2 + sizeof(int16_t) + sizeof(int8_t) * 5);
-                auto dt = v.getDateTime();
-                buf.append(reinterpret_cast<const char*>(&dt.year), sizeof(int16_t))
-                   .append(reinterpret_cast<const char*>(&dt.month), sizeof(int8_t))
-                   .append(reinterpret_cast<const char*>(&dt.day), sizeof(int8_t))
-                   .append(reinterpret_cast<const char*>(&dt.hour), sizeof(int8_t))
-                   .append(reinterpret_cast<const char*>(&dt.minute), sizeof(int8_t))
-                   .append(reinterpret_cast<const char*>(&dt.sec), sizeof(int8_t))
-                   .append(reinterpret_cast<const char*>(&dt.microsec), sizeof(int32_t))
-                   .append(reinterpret_cast<const char*>(&dt.timezone), sizeof(int32_t));
-                return buf;
-            }
-            default :
-                LOG(ERROR) << "Unsupported default value type";
-        }
-        return "";
+        key.append(vId.data(), vId.size())
+           .append(vIdLen - vId.size(), '\0');
+        return key;
     }
 
     /**
-     * Default, positive number first bit is 0, negative number is 1 .
-     * To keep the string in order, the first bit must to be inverse.
-     * for example as below :
-     *    9223372036854775807     -> "\377\377\377\377\377\377\377\377"
-     *    1                       -> "\200\000\000\000\000\000\000\001"
-     *    0                       -> "\200\000\000\000\000\000\000\000"
-     *    -1                      -> "\177\377\377\377\377\377\377\377"
-     *    -9223372036854775808    -> "\000\000\000\000\000\000\000\000"
-     */
-
-    static std::string encodeInt64(int64_t v) {
-        v ^= folly::to<int64_t>(1) << 63;
-        auto val = folly::Endian::big(v);
-        std::string raw;
-        raw.reserve(sizeof(int64_t));
-        raw.append(reinterpret_cast<const char*>(&val), sizeof(int64_t));
-        return raw;
-    }
-
-    static int64_t decodeInt64(const folly::StringPiece& raw) {
-        auto val = *reinterpret_cast<const int64_t*>(raw.data());
-        val = folly::Endian::big(val);
-        val ^= folly::to<int64_t>(1) << 63;
-        return val;
-    }
-
-    /*
-     * Default, the double memory structure is :
-     *   sign bit（1bit）+  exponent bit(11bit) + float bit(52bit)
-     *   The first bit is the sign bit, 0 for positive and 1 for negative
-     *   To keep the string in order, the first bit must to be inverse,
-     *   then need to subtract from maximum.
-     */
-
-    static std::string encodeDouble(double v) {
-        if (v < 0) {
-            /**
-             *   TODO : now, the -(std::numeric_limits<double>::min())
-             *   have a problem of precision overflow. current return value is -nan.
-             */
-            auto i = *reinterpret_cast<const int64_t*>(&v);
-            i = -(std::numeric_limits<int64_t>::max() + i);
-            v = *reinterpret_cast<const double*>(&i);
+     * param valueTypes ： column type of each index column. If there are no nullable columns
+     *                     in the index, the parameter can be empty.
+     **/
+    static std::string edgeIndexKey(size_t vIdLen, PartitionID partId,
+                                    IndexID indexId, VertexID srcId,
+                                    EdgeRanking rank, VertexID dstId,
+                                    const std::vector<Value>& values,
+                                    const std::vector<Value::Type>& valueTypes = {}) {
+        int32_t item = (partId << kPartitionOffset) | static_cast<uint32_t>(NebulaKeyType::kIndex);
+        std::string key;
+        key.reserve(256);
+        key.append(reinterpret_cast<const char*>(&item), sizeof(int32_t))
+           .append(reinterpret_cast<const char*>(&indexId), sizeof(IndexID));
+        // If have not nullable columns in the index, the valueTypes is empty.
+        if (valueTypes.empty()) {
+            encodeValues(values, key);
+        } else {
+            encodeValuesWithNull(values, valueTypes, key);
         }
-        auto val = folly::Endian::big(v);
-        auto* c = reinterpret_cast<char*>(&val);
-        c[0] ^= 0x80;
-        std::string raw;
-        raw.reserve(sizeof(double));
-        raw.append(c, sizeof(double));
-        return raw;
-    }
-
-    static double decodeDouble(const folly::StringPiece& raw) {
-        char* v = const_cast<char*>(raw.data());
-        v[0] ^= 0x80;
-        auto val = *reinterpret_cast<const double*>(v);
-        val = folly::Endian::big(val);
-        if (val < 0) {
-            auto i = *reinterpret_cast<const int64_t*>(&val);
-            i = -(std::numeric_limits<int64_t >::max() + i);
-            val = *reinterpret_cast<const double*>(&i);
-        }
-        return val;
-    }
-
-    static StatusOr<Value> decodeValue(const folly::StringPiece& raw, Value::Type type) {
-        Value v;
-        switch (type) {
-            case Value::Type::INT : {
-                v.setInt(decodeInt64(raw));
-                break;
-            }
-            case Value::Type::FLOAT : {
-                v.setFloat(decodeDouble(raw));
-                break;
-            }
-            case Value::Type::BOOL : {
-                v.setBool(*reinterpret_cast<const bool*>(raw.data()));
-                break;
-            }
-            case Value::Type::STRING : {
-                v.setStr(raw.str());
-                break;
-            }
-            case Value::Type::DATE: {
-                nebula::Date dt;
-                memcpy(reinterpret_cast<void*>(&dt.year), &raw[0], sizeof(int16_t));
-                memcpy(reinterpret_cast<void*>(&dt.month),
-                       &raw[sizeof(int16_t)],
-                       sizeof(int8_t));
-                memcpy(reinterpret_cast<void*>(&dt.day),
-                       &raw[sizeof(int16_t) + sizeof(int8_t)],
-                       sizeof(int8_t));
-                v.setDate(dt);
-                break;
-            }
-            case Value::Type::DATETIME: {
-                nebula::DateTime dt;
-                memcpy(reinterpret_cast<void*>(&dt.year), &raw[0], sizeof(int16_t));
-                memcpy(reinterpret_cast<void*>(&dt.month),
-                       &raw[sizeof(int16_t)],
-                       sizeof(int8_t));
-                memcpy(reinterpret_cast<void*>(&dt.day),
-                       &raw[sizeof(int16_t) + sizeof(int8_t)],
-                       sizeof(int8_t));
-                memcpy(reinterpret_cast<void*>(&dt.hour),
-                       &raw[sizeof(int16_t) + 2 * sizeof(int8_t)],
-                       sizeof(int8_t));
-                memcpy(reinterpret_cast<void*>(&dt.minute),
-                       &raw[sizeof(int16_t) + 3 * sizeof(int8_t)],
-                       sizeof(int8_t));
-                memcpy(reinterpret_cast<void*>(&dt.sec),
-                       &raw[sizeof(int16_t) + 4 * sizeof(int8_t)],
-                       sizeof(int8_t));
-                memcpy(reinterpret_cast<void*>(&dt.microsec),
-                       &raw[sizeof(int16_t) + 5 * sizeof(int8_t)],
-                       sizeof(int32_t));
-                memcpy(reinterpret_cast<void*>(&dt.timezone),
-                       &raw[sizeof(int16_t) + 5 * sizeof(int8_t) + sizeof(int32_t)],
-                       sizeof(int32_t));
-                v.setDateTime(dt);
-                break;
-            }
-            default:
-                return Status::Error("Unknown value type");
-        }
-        return v;
+        key.append(srcId.data(), srcId.size())
+           .append(vIdLen - srcId.size(), '\0')
+           .append(reinterpret_cast<const char*>(&rank), sizeof(EdgeRanking))
+           .append(dstId.data(), dstId.size())
+           .append(vIdLen - dstId.size(), '\0');
+        return key;
     }
 
     static VertexIDSlice getIndexVertexID(size_t vIdLen, const folly::StringPiece& rawKey) {
@@ -296,35 +110,56 @@ public:
         return readInt<IndexID>(rawKey.data() + offset, sizeof(IndexID));
     }
 
-    /**
-     * Generate vertex|edge index key for kv store
-     **/
+    static std::string indexPrefix(PartitionID partId, IndexID indexId) {
+        PartitionID item = (partId << kPartitionOffset)
+                           | static_cast<uint32_t>(NebulaKeyType::kIndex);
+        std::string key;
+        key.reserve(sizeof(PartitionID) + sizeof(IndexID));
+        key.append(reinterpret_cast<const char*>(&item), sizeof(PartitionID))
+           .append(reinterpret_cast<const char*>(&indexId), sizeof(IndexID));
+        return key;
+    }
+
     static void encodeValues(const std::vector<Value>& values, std::string& raw);
 
     static void encodeValuesWithNull(const std::vector<Value>& values,
                                      const std::vector<Value::Type>& colsType,
                                      std::string& raw);
 
-    /**
-     * param valueTypes ： column type of each index column. If there are no nullable columns
-     *                     in the index, the parameter can be empty.
-     **/
-    static std::string vertexIndexKey(size_t vIdLen, PartitionID partId,
-                                      IndexID indexId, VertexID vId,
-                                      const std::vector<Value>& values,
-                                      const std::vector<Value::Type>& valueTypes = {});
+    static Value::Type toValueType(PropertyType type);
+
+    static std::string encodeNullValue(Value::Type type);
+
+    static std::string encodeValue(const Value& v);
 
     /**
-     * param valueTypes ： column type of each index column. If there are no nullable columns
-     *                     in the index, the parameter can be empty.
-     **/
-    static std::string edgeIndexKey(size_t vIdLen, PartitionID partId,
-                                    IndexID indexId, VertexID srcId,
-                                    EdgeRanking rank, VertexID dstId,
-                                    const std::vector<Value>& values,
-                                    const std::vector<Value::Type>& valueTypes = {});
+     * Default, positive number first bit is 0, negative number is 1 .
+     * To keep the string in order, the first bit must to be inverse.
+     * for example as below :
+     *    9223372036854775807     -> "\377\377\377\377\377\377\377\377"
+     *    1                       -> "\200\000\000\000\000\000\000\001"
+     *    0                       -> "\200\000\000\000\000\000\000\000"
+     *    -1                      -> "\177\377\377\377\377\377\377\377"
+     *    -9223372036854775808    -> "\000\000\000\000\000\000\000\000"
+     */
 
-    static std::string indexPrefix(PartitionID partId, IndexID indexId);
+    static std::string encodeInt64(int64_t v);
+
+    static int64_t decodeInt64(const folly::StringPiece& raw);
+
+    /*
+     * Default, the double memory structure is :
+     *   sign bit（1bit）+  exponent bit(11bit) + float bit(52bit)
+     *   The first bit is the sign bit, 0 for positive and 1 for negative
+     *   To keep the string in order, the first bit must to be inverse,
+     *   then need to subtract from maximum.
+     */
+
+    static std::string encodeDouble(double v);
+
+    static double decodeDouble(const folly::StringPiece& raw);
+
+    static StatusOr<Value> decodeValue(const folly::StringPiece& raw, Value::Type type);
 
 private:
     IndexKeyUtils() = delete;
