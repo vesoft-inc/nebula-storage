fe7547f687c4689c2d9ea4353cc369a1262c316e
panda-sheep <59197347+panda-sheep@users.noreply.github.com>
2021-02-05 18:44:35 +0800

    address critical27's comments

--- src/kvstore/RocksEngine.cpp
+++ src/kvstore/RocksEngine.cpp
@@ -363,10 +363,8 @@ ResultCode RocksEngine::setDBOption(const std::string& configKey, const std::str
 
 ResultCode RocksEngine::compact() {
     rocksdb::CompactRangeOptions options;
-    // If change_level is true and target_level is -1,
-    // compacted files will be moved to the minimum level capable of holding the data.
-    options.change_level = true;
-    options.target_level = -1;
+    options.change_level = FLAGS_rocksdb_compact_change_level;
+    options.target_level = FLAGS_rocksdb_compact_target_level;
     rocksdb::Status status = db_->CompactRange(options, nullptr, nullptr);
     if (status.ok()) {
         return ResultCode::SUCCEEDED;
