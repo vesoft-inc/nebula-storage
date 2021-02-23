a7fb1995be4337f967dfedfcd6f2c7cb7b40d752
liuyu <52276794+liuyu85cn@users.noreply.github.com>
2021-02-23 15:58:04 +0800

    iterator good to go

--- src/storage/transaction/TossNoVerEdgeIterator.h
+++ src/storage/transaction/TossNoVerEdgeIterator.h
@@ -27,26 +27,21 @@ class PendingLockIter {
 public:
     PendingLockIter(Container& locks, DataChecker checker)
         : locks_(locks), dataChecker_(std::move(checker)) {
-        LOG(INFO) << "PendingLockIter::ctor(), locks_.size() = " << locks_.size();
         curr_ = locks_.begin();
         if (!check()) {
             next();
         }
-        LOG(INFO) << "~PendingLockIter::ctor()";
     }
 
     bool valid() const {
-        LOG(INFO) << "PendingLockIter::valid()";
         if (locks_.empty()) {
             return false;
         }
         auto valid = curr_ != locks_.end();
-        LOG(INFO) << "~PendingLockIter::valid(), valid = " << valid;
         return valid;
     }
 
     bool check() {
-        LOG(INFO) << "enter PendingLockIter::check()";
         if (!valid()) {
             return false;
         }
@@ -61,49 +56,12 @@ public:
         return true;
     }
 
-    // bool check() {
-    //     LOG(INFO) << "PendingLockIter::check()";
-    //     bool ret = false;
-    //     do {
-    //         if (!valid()) {
-    //             LOG(INFO) << "!valid()";
-    //             break;
-    //         }
-    //         if (emptyPendLock(curr_->get())) {
-    //             LOG(INFO) << "emptyPendLock(curr_->get()";
-    //             break;
-    //         }
-    //         auto& lock = *(*curr_);
-    //         if (lock.lockProps) {
-    //             LOG(INFO) << "lock.lockProps is true " << folly::hexlify(*lock.lockProps);
-    //         } else {
-    //             LOG(INFO) << "lock.lockProps is false ";
-    //         }
-    //         if (lock.edgeProps) {
-    //             LOG(INFO) << "lock.edgeProps is true " << folly::hexlify(*lock.edgeProps);
-    //         } else {
-    //             LOG(INFO) << "lock.edgeProps is false ";
-    //         }
-    //         auto& val = lock.lockProps ? *lock.lockProps : *lock.edgeProps;
-    //         if (!dataChecker_(val)) {
-    //             LOG(INFO) << "!dataChecker_(val)";
-    //             break;
-    //         }
-    //         ret = true;
-    //     } while(0);
-
-    //     LOG(INFO) << "~PendingLockIter::check(), ret = " << ret;
-    //     return ret;
-    // }
-
     void next() {
-        LOG(INFO) << "PendingLockIter::next()";
         for (++curr_; valid(); ++curr_) {
             if (check()) {
                 break;
             }
         }
-        LOG(INFO) << "~PendingLockIter::next()";
     }
 
     bool emptyPendLock(PendingLock* pendingLock) {
@@ -112,7 +70,6 @@ public:
 
     folly::StringPiece key() {
         std::shared_ptr<PendingLock> splk = *curr_;
-        LOG(INFO) << "PendingLockIter::key()=" << folly::hexlify(splk->lockKey);
         return splk->lockKey;
     }
 
@@ -132,8 +89,6 @@ public:
         const folly::Optional<std::pair<std::string, int64_t>>* ttl,
         bool stopAtFirstEdge)
         : SingleEdgeIterator() {
-        LOG(INFO) << "TossNoVerEdgeIterator::ctor(), iter->key=" << folly::hexlify(iter->key());
-        LOG(INFO) << boost::stacktrace::stacktrace();
         planContext_ = planCtx;
         iter_ = std::move(iter);
         edgeType_ = edgeType;
@@ -147,23 +102,17 @@ public:
         if (!checkIter()) {
             next();
         }
-        LOG(INFO) << "~TossNoVerEdgeIterator::ctor()";
     }
 
     folly::StringPiece key() const override {
-        LOG(INFO) << "enter TossNoVerEdgeIterator::key()";
         return iter_->valid() ? iter_->key() : lockIter_->key();
     }
 
     bool valid() const override {
-        LOG(INFO) << "TossNoVerEdgeIterator::valid()";
-        bool valid = iter_->valid() || lockIter_->valid();
-        LOG(INFO) << "~TossNoVerEdgeIterator::valid(), valid = " << valid;
-        return valid;
+        return iter_->valid() || lockIter_->valid();
     }
 
     void next() override {
-        LOG(INFO) << "TossNoVerEdgeIterator::next()";
         if (stopSearching_) {
             return;
         }
@@ -177,30 +126,12 @@ public:
         }
 
         if (!iter_->valid()) {
-            LOG(INFO) << "TossNoVerEdgeIterator::next(), !iter_->valid()";
             if (!lockIter_) {
                 if (!resumeTasks_.empty()) {
                     folly::collectAll(resumeTasks_).wait();
                 }
-                auto dataChecker = [&](folly::StringPiece val) {
-                    LOG(INFO) << "dataChecker() val=" << folly::hexlify(val);
-                    reader_.reset(*schemas_, val);
-                    if (!reader_) {
-                        planContext_->resultStat_ = ResultStatus::ILLEGAL_DATA;
-                        return false;
-                    }
-
-                    if (hasTtl_ &&
-                        CommonUtils::checkDataExpiredForTTL(
-                            schemas_->back().get(), reader_.get(), ttlCol_, ttlDuration_)) {
-                        reader_.reset();
-                        return false;
-                    }
-
-                    return true;
-                };
-                lockIter_ =
-                    std::make_unique<PendingLockIter>(pendingLocks_, std::move(dataChecker));
+                lockIter_ = std::make_unique<PendingLockIter>(
+                    pendingLocks_, [&](auto val) { return checkValue(val); });
             } else {
                 if (lockIter_->valid()) {
                     lockIter_->next();
@@ -213,13 +144,13 @@ public:
     // and if it points to a lock, try to resume it.
     bool checkIter() {
         if (NebulaKeyUtils::isLock(planContext_->vIdLen_, iter_->key())) {
-            LOG(INFO) << "meet a lock: " << folly::hexlify(iter_->key());
+            LOG_IF(INFO, FLAGS_trace_toss) << "meet a lock: " << folly::hexlify(iter_->key());
             pendingLocks_.emplace_back(std::make_shared<PendingLock>(iter_->key()));
             resumeTasks_.emplace_back(
                 planContext_->env_->txnMan_->resumeLock(planContext_, pendingLocks_.back()));
             checkEdgeHasLock_ = true;
         } else if (NebulaKeyUtils::isEdge(planContext_->vIdLen_, iter_->key())) {
-            LOG(INFO) << "meet an edge: " << folly::hexlify(iter_->key());
+            LOG_IF(INFO, FLAGS_trace_toss) << "meet an edge: " << folly::hexlify(iter_->key());
             if (hasPendingLock(iter_->key())) {
                 pendingLocks_.back()->edgeProps = iter_->val().str();
                 return false;
@@ -233,8 +164,22 @@ public:
         return false;
     }
 
+    bool checkValue(folly::StringPiece val) {
+        reader_.reset(*schemas_, val);
+        if (!reader_) {
+            planContext_->resultStat_ = ResultStatus::ILLEGAL_DATA;
+            return false;
+        }
+
+        if (hasTtl_ && CommonUtils::checkDataExpiredForTTL(
+                           schemas_->back().get(), reader_.get(), ttlCol_, ttlDuration_)) {
+            reader_.reset();
+            return false;
+        }
+        return true;
+    }
+
     bool hasPendingLock(const folly::StringPiece& ekey) {
-        LOG(INFO) << "enter TossNoVerEdgeIterator::nextEdge()";
         if (!checkEdgeHasLock_) {
             return false;
         }
@@ -246,12 +191,12 @@ public:
     }
 
 private:
-    bool                                                 stopAtFirstEdge_{false};
-    std::list<std::shared_ptr<PendingLock>> pendingLocks_;
-    std::list<folly::SemiFuture<cpp2::ErrorCode>> resumeTasks_;
-    std::unique_ptr<PendingLockIter> lockIter_;
+    bool                                                    stopAtFirstEdge_{false};
+    std::list<std::shared_ptr<PendingLock>>                 pendingLocks_;
+    std::list<folly::SemiFuture<cpp2::ErrorCode>>           resumeTasks_;
+    std::unique_ptr<PendingLockIter>                        lockIter_;
     // use this for fast check if an edge has a lock(by avoid strcmp).
-    bool checkEdgeHasLock_{false};
+    bool                                                    checkEdgeHasLock_{false};
 };
 
 }   // namespace storage
