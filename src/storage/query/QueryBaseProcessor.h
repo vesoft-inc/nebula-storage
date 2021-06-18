58228920381cb9c28fad9e0bca745963185f09fe
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2021-06-18 16:32:42 +0800

    Check the vid when get vertex properties.

--- src/storage/query/QueryBaseProcessor.h
+++ src/storage/query/QueryBaseProcessor.h
@@ -7,6 +7,7 @@
 #ifndef STORAGE_QUERY_QUERYBASEPROCESSOR_H_
 #define STORAGE_QUERY_QUERYBASEPROCESSOR_H_
 
+#include <vector>
 #include "common/base/Base.h"
 #include "common/context/ExpressionContext.h"
 #include "common/expression/Expression.h"
@@ -161,7 +162,8 @@ protected:
     nebula::cpp2::ErrorCode getSpaceEdgeSchema();
 
     // build tagContexts_ according to return props
-    nebula::cpp2::ErrorCode handleVertexProps(std::vector<cpp2::VertexProp>& tagProps);
+    nebula::cpp2::ErrorCode handleVertexProps(std::vector<cpp2::VertexProp>& tagProps,
+                                              const std::vector<TagID>& lackTags = {});
     // build edgeContexts_ according to return props
     nebula::cpp2::ErrorCode handleEdgeProps(std::vector<cpp2::EdgeProp>& edgeProps);
 
@@ -174,6 +176,8 @@ protected:
 
     std::vector<cpp2::VertexProp> buildAllTagProps();
     std::vector<cpp2::EdgeProp> buildAllEdgeProps(const cpp2::EdgeDirection& direction);
+
+    std::vector<TagID> lackTag(const std::vector<cpp2::VertexProp>& tagProps);
 
     nebula::cpp2::ErrorCode
     checkExp(const Expression* exp,
